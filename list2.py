8ffce35f2c9078a963d8a188a469821047022617
IambicTrochee <sally.luong@gmail.com>
2017-03-27 13:35:55 -0400

    Lists 2 Completed

--- list2.py
+++ list2.py
@@ -27,8 +27,9 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+  for i in range(len(list2)):
+      list1.append(list2[i])
+  return sorted(list1)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
